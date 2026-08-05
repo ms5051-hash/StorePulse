daaf1eb4dc3aa801f39696610ec5353499a440b7
Matthew <mszeto007@gmail.com>
2026-08-05 00:49:41 -0400

    fixed the order of the code to make it more readable

--- scripts/create_dimensions.py
+++ scripts/create_dimensions.py
@@ -12,16 +12,22 @@ print("Customer dimension created")
 #dimension product
 
 product_df = df[["StockCode", "Description"]]
-product_df = product_df.drop_duplicates()
+
+product_df = product_df.drop_duplicates(
+    subset=["StockCode"]
+)
+
 product_df = product_df.reset_index(drop=True)
 product_df.insert(0, "product_id", product_df.index + 1)
 product_df.to_csv("data/dim_product.csv", index=False)
 print("Product dimension created")
 
 date_df = df[["InvoiceDate"]]
+date_df["InvoiceDate"] = pd.to_datetime(date_df["InvoiceDate"])
 date_df = date_df.drop_duplicates()
 date_df = date_df.reset_index(drop=True)
-date_df["InvoiceDate"] = pd.to_datetime(date_df["InvoiceDate"])
+
+date_df.insert(0, "date_id", date_df.index + 1)
 
 # Create extra date columns
 date_df["Year"] = date_df["InvoiceDate"].dt.year
@@ -29,8 +35,6 @@ date_df["Month"] = date_df["InvoiceDate"].dt.month
 date_df["Day"] = date_df["InvoiceDate"].dt.day
 date_df["Hour"] = date_df["InvoiceDate"].dt.hour
 
-
 date_df.to_csv("data/dim_date.csv", index=False)
-
 
 print("Date dimension created!")
